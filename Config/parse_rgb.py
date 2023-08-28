c1030fc701c19498cca3c9b6ec9a30943e196f8b
00008156 <david.hofheinz@posteo.de>
2023-08-28 18:18:44 +0200

    commit

--- Config/parse_rgb.py
+++ Config/parse_rgb.py
@@ -1,6 +1,6 @@
-def parse_rbg(red: int, green: int, blue: int, alpha: float) -> tuple:
+def parse_rgb(red: int, green: int, blue: int, alpha: float) -> tuple:
     red: float = red / 255
     green: float = green / 255
     blue: float = blue / 255
     alpha: float = alpha
-    return red, blue, green, alpha
+    return red, green, blue, alpha
--- myapp.py
+++ myapp.py
@@ -3,15 +3,28 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.graphics import Color, Rectangle
 from kivy.core.window import Window
 
+from Config import parse_rgb
 
 class MyGlobals:
     global_var = "12"
 
 
 class MainInterface(BoxLayout):
+    global_red = parse_rgb.parse_rgb(255, 0, 0, 1)
+    global_green = parse_rgb.parse_rgb(0, 255, 0, 1)
+    global_blue = parse_rgb.parse_rgb(0, 0, 255, 1)
+    global_orange = parse_rgb.parse_rgb(153, 76, 0, 1)
+    global_white = parse_rgb.parse_rgb(255, 255, 255, 0.5)
+
     def __init__(self, **kwargs):
         super(MainInterface, self).__init__(**kwargs)
-        self.my_globals = "test"
+
+
+    def button_pressed(self):
+        #open popup a window with some stuff to wirte in
+
+        pass
+
 
 
 class MyApp(App):
